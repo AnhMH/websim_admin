49e8fe792fffcb4c4d7ccd0a60d46ba750e5e469
AnhMH <anhmh@lifull-tech.vn>
2018-06-03 01:34:27 +0700

    Add tags

--- config/apiurl.php
+++ config/apiurl.php
@@ -21,11 +21,16 @@ Configure::write('API.url_suppliers_all', 'suppliers/all');
 Configure::write('API.url_suppliers_detail', 'suppliers/detail');
 Configure::write('API.url_suppliers_addupdate', 'suppliers/addupdate');
 
-Configure::write('API.url_cates_list', 'cates/list');
-Configure::write('API.url_cates_all', 'cates/all');
-Configure::write('API.url_cates_detail', 'cates/detail');
-Configure::write('API.url_cates_addupdate', 'cates/addupdate');
+Configure::write('API.url_tags_list', 'tags/list');
+Configure::write('API.url_tags_all', 'tags/all');
+Configure::write('API.url_tags_detail', 'tags/detail');
+Configure::write('API.url_tags_addupdate', 'tags/addupdate');
 
 Configure::write('API.url_products_list', 'products/list');
 Configure::write('API.url_products_detail', 'products/detail');
 Configure::write('API.url_products_addupdate', 'products/addupdate');
+
+Configure::write('API.url_cates_list', 'cates/list');
+Configure::write('API.url_cates_all', 'cates/all');
+Configure::write('API.url_cates_detail', 'cates/detail');
+Configure::write('API.url_cates_addupdate', 'cates/addupdate');
---
+++ src/Controller/Bus/Tags/update.php
@@ -0,0 +1,84 @@
+<?php
+
+use App\Form\UpdateTagForm;
+use App\Lib\Api;
+use Cake\Core\Configure;
+use Cake\Network\Exception\NotFoundException;
+
+// Load detail
+$data = null;
+if (!empty($id)) {
+    // Edit
+    $param['id'] = $id;
+    $data = Api::call(Configure::read('API.url_tags_detail'), $param);
+    $this->Common->handleException(Api::getError());
+    if (empty($data)) {
+        AppLog::info("Tag unavailable", __METHOD__, $param);
+        throw new NotFoundException("Tag unavailable", __METHOD__, $param);
+    }
+
+    $pageTitle = __('LABEL_TAG_UPDATE');
+} else {
+    // Create new
+    $pageTitle = __('LABEL_ADD_NEW');
+}
+
+// Create breadcrumb
+$listPageUrl = h($this->BASE_URL . '/tags');
+$this->Breadcrumb->setTitle($pageTitle)
+        ->add(array(
+            'link' => $listPageUrl,
+            'name' => __('LABEL_TAG_LIST'),
+        ))
+        ->add(array(
+            'name' => $pageTitle,
+        ));
+// Create Update form 
+$form = new UpdateTagForm();
+$this->UpdateForm->reset()
+    ->setModel($form)
+    ->setData($data)
+    ->setAttribute('autocomplete', 'off')
+    ->addElement(array(
+        'id' => 'id',
+        'type' => 'hidden',
+        'label' => __('id'),
+    ))
+    ->addElement(array(
+        'id' => 'name',
+        'label' => __('LABEL_NAME'),
+        'required' => true,
+    ))
+    ->addElement(array(
+        'type' => 'submit',
+        'value' => __('LABEL_SAVE'),
+        'class' => 'btn btn-primary',
+    ))
+    ->addElement(array(
+        'type' => 'submit',
+        'value' => __('LABEL_CANCEL'),
+        'class' => 'btn',
+        'onclick' => "return back();"
+    ));
+
+// Valdate and update
+if ($this->request->is('post')) {
+    // Trim data
+    $data = $this->request->data();
+    foreach ($data as $key => $value) {
+        if (is_scalar($value)) {
+            $data[$key] = trim($value);
+        }
+    }
+    // Validation
+    if ($form->validate($data)) {
+        // Call API
+        $id = Api::call(Configure::read('API.url_tags_addupdate'), $data);
+        if (!empty($id) && !Api::getError()) {
+            $this->Flash->success(__('MESSAGE_SAVE_OK'));
+            return $this->redirect("{$this->BASE_URL}/{$this->controller}/update/{$id}");
+        } else {
+            return $this->Flash->error(__('MESSAGE_SAVE_NG'));
+        }
+    }
+}
