21c486db49f11dfd75f9f026205cca3589a90e3d
AnhMH <anhmh@lifull-tech.vn>
2018-08-05 11:06:52 +0700

    Add production config

--- config/production/bootstrap.php
+++ config/production/bootstrap.php
@@ -7,8 +7,8 @@ use Cake\Core\Configure;
 
 define('USE_SUB_DIRECTORY', '');
 
-Configure::write('API.Host', '');
-Configure::write('Config.HTTPS', true);
+Configure::write('API.Host', 'http://apiwebsim.hoanganhonline.com/public/');
+Configure::write('Config.HTTPS', false);
 
 Configure::write('Config.CKeditor', array(
     'basel_dir'=>'/var/www/img/uploads/',
